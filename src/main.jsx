67997f0298f5e76fbd2f7e410c62243a1a9c230c
Sourabh Singh <specious0027@gmail.com>
2025-10-17 13:45:53 +0530

    first update_1

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
 
+if (typeof window !== 'undefined') {
+  const configuredBase = import.meta?.env?.VITE_API_BASE_URL ?? 'http://192.168.50.1:8000'
+  window.__API_BASE__ = String(configuredBase).replace(/\/$/, '')
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
